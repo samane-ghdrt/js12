8dfd6a0019c22abdef361d368c915e56f3c4da05
samane ghodrati <samane.ghodrati@gmail.com>
2024-12-13 12:57:35 -0900

    add

--- JS-12-Q5.js
+++ JS-12-Q5.js
@@ -3,11 +3,14 @@
 //todo2: Calculate the length of each element in the array.
 //todo3: Return a new array containing the lengths of the elements.
 
-// const obj = {
-//   string: "Hello world how are you?",
-//   write code here
-// }
+const obj = {
+  string: "Hello world how are you?",
+  counterString(string){
+    return string.split(' ').map(word=>word.length);
+  }
+}
 
 // note: obj.counterString("Hello world how are you?")==>[ 5, 5, 3, 3, 4 ]
 
 // !Answer:
+console.log(obj.counterString("Hello world how are you?"));
--- js-12-Q6.js
+++ js-12-Q6.js
@@ -3,3 +3,12 @@
 // ToDo-2: Write a method for this object; this method must print 'string' in the console. Like => `Hello, my name is {name for example Jasem}, and I am {age for example 120} years old.`
 // hint: To access properties in the method you must use "this" keyword.
 // !Answer:
+const obj1 ={
+    name:'saba',
+    age:24,
+    hellow(){
+        console.log(`Hello, my name is ${this.name}, and I am ${this.age} years old.`);
+    }
+}
+
+obj1.hellow();
